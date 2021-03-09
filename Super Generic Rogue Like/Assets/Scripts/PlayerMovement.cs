506360c40d89f0e5fd6ef2dde03da90558aa3387
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2021-03-09 10:02:44 +0000

    Added movement script

---
+++ Super Generic Rogue Like/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if game is paused, if paused, do not allow movement
+        Debug.DrawLine(transform.position, transform.position + Vector3.right, Color.white);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            transform.position += Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            }
+            transform.position += Vector3.right;
+        }
+
+    }
