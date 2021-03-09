1c747f53a245476955aef0b9ee495093f6fcde77
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2021-03-09 14:52:19 +0000

    Tilemap testing, code changes to collide with tilemaps.

--- Super Generic Rogue Like/Assets/Scripts/EnemyRandomMovement.cs
+++ Super Generic Rogue Like/Assets/Scripts/EnemyRandomMovement.cs
@@ -6,6 +6,7 @@ public class EnemyRandomMovement : MonoBehaviour
 {
     PlayerMovement Player;
     System.Random Rand = new System.Random();
+    RaycastHit2D hit;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,34 +18,66 @@ public class EnemyRandomMovement : MonoBehaviour
     {
         if (!Player.getTurn())
         {
-            int Direction = Rand.Next(0, 3);
-            switch (Direction)
+            while (!Player.getTurn())
             {
-                case 0:
-                    {
-                        transform.position += Vector3.up;
-                        Player.setTurn(true);
-                        return;
-                    }
-                case 1:
-                    {
-                        transform.position += Vector3.left;
-                        Player.setTurn(true);
-                        return;
-                    }
-                case 2:
-                    {
-                        transform.position += Vector3.down;
-                        Player.setTurn(true);
-                        return;
-                    }
-                case 3:
-                    {
-                        transform.position += Vector3.right;
-                        Player.setTurn(true);
-                        return;
-                    }
+                int Direction = Rand.Next(0, 3);
+                switch (Direction)
+                {
+                    case 0:
+                        {
+                            if (hit = Physics2D.Raycast(transform.position + Vector3.up, Vector2.up, 0.5f))
+                            {
+                                if (hit.collider.gameObject.tag == "BlockTerrain")
+                                {
+                                    return;
+                                }
+                            }
+                            transform.position += Vector3.up;
+                            Player.setTurn(true);
+                            return;
+                        }
+                    case 1:
+                        {
+                            if (hit = Physics2D.Raycast(transform.position + Vector3.left, Vector2.left, 0.5f))
+                            {
+                                if (hit.collider.gameObject.tag == "BlockTerrain")
+                                {
+                                    return;
+                                }
+                            }
+                            transform.position += Vector3.left;
+                            Player.setTurn(true);
+                            return;
+                        }
+                    case 2:
+                        {
+                            if (hit = Physics2D.Raycast(transform.position + Vector3.down, Vector2.down, 0.5f))
+                            {
+                                if (hit.collider.gameObject.tag == "BlockTerrain")
+                                {
+                                    return;
+                                }
+                            }
+                            transform.position += Vector3.down;
+                            Player.setTurn(true);
+                            return;
+                        }
+                    case 3:
+                        {
+                            if (hit = Physics2D.Raycast(transform.position + Vector3.right, Vector2.right, 0.5f))
+                            {
+                                if (hit.collider.gameObject.tag == "BlockTerrain")
+                                {
+                                    return;
+                                }
+                            }
+                            transform.position += Vector3.right;
+                            Player.setTurn(true);
+                            return;
+                        }
+                }
             }
+           
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
--- Super Generic Rogue Like/Assets/Scripts/PlayerMovement.cs
+++ Super Generic Rogue Like/Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     GameObject UI;
     UIController UIController;
-
+    RaycastHit2D hit;
     void Start()
     {
         UIController = UI.GetComponent<UIController>();
@@ -27,39 +27,73 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        if (Turn) //Check if it is the player's turn to move.
+        if (Turn && movedCount < 2) //Check if it is the player's turn to move.
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
+                if (hit = Physics2D.Raycast(transform.position + Vector3.up, Vector2.up, 0.5f))
+                {
+                    if (hit.collider.gameObject.tag == "BlockTerrain")
+                    {
+                        return;
+                    }
+                }
                 transform.position += Vector3.up;
                 movedCount += 1;
                 UIController.subTurnsRemaining(1);
             }
             if (Input.GetKeyDown(KeyCode.A))
             {
+                if (hit = Physics2D.Raycast(transform.position + Vector3.left, Vector2.left, 0.5f))
+                {
+                    Debug.DrawLine(transform.position, transform.position + (1 / 2 * Vector3.left), Color.white);
+                    if (hit.collider.gameObject.tag == "BlockTerrain")
+                    {
+                        return;
+                    }
+                }
                 transform.position += Vector3.left;
                 movedCount += 1;
                 UIController.subTurnsRemaining(1);
             }
             if (Input.GetKeyDown(KeyCode.S))
             {
+                if (hit = Physics2D.Raycast(transform.position + Vector3.down, Vector2.down, 0.5f))
+                {
+                    if (hit.collider.gameObject.tag == "BlockTerrain")
+                    {
+                        return;
+                    }
+                }
                 transform.position += Vector3.down;
                 movedCount += 1;
                 UIController.subTurnsRemaining(1);
             }
             if (Input.GetKeyDown(KeyCode.D))
             {
+                if (hit = Physics2D.Raycast(transform.position + Vector3.right, Vector2.right, 0.5f))
+                {
+                    if (hit.collider.gameObject.tag == "BlockTerrain")
+                    {
+                        return;
+                    }
+                }
                 transform.position += Vector3.right;
                 movedCount += 1;
                 UIController.subTurnsRemaining(1);
             }
             if (movedCount == 2)
             {
-                setTurn(false);
+                StartCoroutine(setTurnDelayed(0.5f));
+                movedCount = 0;
             }
         }
     }
-
+    IEnumerator setTurnDelayed(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        setTurn(false);
+    }
     public void setTurn(bool value)
     {
         if (value)
